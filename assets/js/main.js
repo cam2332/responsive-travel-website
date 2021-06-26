9d8a06ab83fcdce2dc74591fd641095c3df61c35
cam2332 <kam2332@gmail.com>
2021-06-26 17:45:24 +0200

    Add discover section

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,16 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scroll-header')
     }
 })
+
+
+let swiper = new Swiper('.discover__container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0
+    }
+})
